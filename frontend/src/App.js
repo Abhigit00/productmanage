bcf51763d9e7b9a1cc80191b2f0e587d30ff0fe3
Abhigit00 <136364377+Abhigit00@users.noreply.github.com>
2025-02-17 08:32:34 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/home" element={<Home/>} />
+      <Route path="/" element={<Home/>} />
       <Route path='/details' element={<Details/>} />
       </Routes>
     </Router>
